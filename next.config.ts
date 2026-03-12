9a70f4f647eea5a15954c7c690ac63413f7dfdf8
Chanapongpans Submee <116575713+WaSaBiTH@users.noreply.github.com>
2026-03-12 18:43:07 +0700

    DevToolsToggleFix

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  devIndicators: {
+    appIsrStatus: process.env.HIDE_DEV_TOOLS !== 'true',
+    buildActivity: process.env.HIDE_DEV_TOOLS !== 'true',
+  },
+  experimental: {
+    devOverlay: process.env.HIDE_DEV_TOOLS !== 'true',
+  }
 };
 
 export default nextConfig;
